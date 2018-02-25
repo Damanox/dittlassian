8f10326e1ad07937f797bca9d562f6698de93e99
Stas D <iqpirat@gmail.com>
2018-02-25 17:10:29 +0200

    feat: config structure

---
+++ src/dittlassian.Objects/Common/Configuration.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace dittlassian.Objects.Common
+{
+    public class Configuration
+    {
+        [JsonProperty("discord")]
+        public DiscordConfiguration Discord { get; set; }
+
+        [JsonProperty("rules")]
+        public List<Rule> Rules { get; set; }
+    }
+
+    public class DiscordConfiguration
+    {
+    }
+
+    public class Rule
+    {
+        [JsonProperty("source")]
+        public Source Source { get; set; }
+
+        [JsonProperty("condition")]
+        public string Condition { get; set; }
+
+        [JsonProperty("channelIds")]
+        public List<string> ChannelIds { get; set; }
+    }
+}
---
+++ src/dittlassian.Objects/Common/Source.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dittlassian.Objects.Common
+{
+    public enum Source
+    {
+        Bitbucket = 0,
+        Jira = 1
+    }
+}
